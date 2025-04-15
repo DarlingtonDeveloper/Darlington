202034b093543f62c374103617eb5362d20727ce
Mike <mike.s3crypto@gmail.com>
2025-04-15 19:04:15 +0100

    Make the SplineScene Serverside

--- components/SplineSceneBasic.tsx
+++ components/SplineSceneBasic.tsx
@@ -1,20 +1,7 @@
-'use client'
-
 import { Card } from "@/components/ui/card"
-import dynamic from 'next/dynamic'
 import ClientWrapper from "@/components/ClientWrapper"
-import { Typewriter } from "@/components/ui/typewriter"
 import { motion } from "framer-motion"
-
-// Dynamic import with ssr: false
-const SplineScene = dynamic(() => import('@/components/ui/spline-scene'), {
-    ssr: false,
-    loading: () => (
-        <div className="w-full h-full flex items-center justify-center">
-            <div className="w-8 h-8 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
-        </div>
-    )
-})
+import SplineScene from "@/components/ui/spline-scene"
 
 export function SplineSceneBasic() {
     return (
@@ -40,15 +27,6 @@ export function SplineSceneBasic() {
                                     <span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
                                         Darlington
                                     </span>
-                                    <Typewriter
-                                        text={[".Architect", ".Developer", ".Designer", ".Engineer"]}
-                                        speed={70}
-                                        className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
-                                        waitTime={1500}
-                                        deleteSpeed={40}
-                                        cursorChar={"_"}
-                                        cursorClassName="text-neutral-400 ml-0.5"
-                                    />
                                 </h1>
                             </motion.div>
                         </div>
--- components/ui/spline-scene.tsx
+++ components/ui/spline-scene.tsx
@@ -1,16 +1,4 @@
-/* eslint-disable import/no-unresolved */
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const Spline = dynamic(() => import('@splinetool/react-spline').then(mod => mod.default), {
-    ssr: false,
-    loading: () => (
-        <div className="w-full h-full flex items-center justify-center">
-            <div className="w-8 h-8 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
-        </div>
-    ),
-});
+import Spline from '@splinetool/react-spline/next';
 
 interface SplineSceneProps {
     scene: string;
