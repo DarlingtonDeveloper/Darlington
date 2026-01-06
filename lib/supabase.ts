9d5d8160cfe4fb5b8bb5d74f37b367f0a4d10963
Mike <mike.s3crypto@gmail.com>
2026-01-06 12:06:22 +0700

    keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,22 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-// Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = url && key ? createClient(url, key) : null as any
 
-// For server-side use with service role key (bypasses RLS)
 export const supabaseAdmin =
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-    ? createClient(
-      supabaseUrl,
-      process.env.SUPABASE_SERVICE_ROLE_KEY,
-      {
-        auth: {
-          autoRefreshToken: false,
-          persistSession: false,
-        },
-      }
-    )
+  url && process.env.SUPABASE_SERVICE_ROLE_KEY
+    ? createClient(url, process.env.SUPABASE_SERVICE_ROLE_KEY, {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    })
     : null
